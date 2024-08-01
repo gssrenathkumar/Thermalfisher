5df8f7caf76126f4f01cfe9fd7309c21ca79f2ff
G S SRENATH KUMAR <70089532+gssrenathkumar@users.noreply.github.com>
2024-08-01 12:10:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,14 @@ import pandas as pd
 import pickle
 from sklearn.preprocessing import LabelEncoder
 
-# Paths to your models and CSV file
-model_paths = {
-    'CatBoost': 'pickle_file/best_catboost_model.pkl',
-    # 'LightGBM': 'pickle_file/best_lgbm_model.pkl',
-    # 'Random Forest': 'pickle_file/best_rf_model.pkl'
-}
-csv_file_path = 'cluster_family_reshare.csv'
+# Path to the CatBoost model
+catboost_model_path = 'pickle_file/best_catboost_model.pkl'
 
-# Load all models
-models = {name: pickle.load(open(path, 'rb')) for name, path in model_paths.items()}
+# Load the CatBoost model
+catboost_model = pickle.load(open(catboost_model_path, 'rb'))
 
 # Load the CSV file
+csv_file_path = 'cluster_family_reshare.csv'
 df_clusters = pd.read_csv(csv_file_path)
 
 # List of old_node_type values
@@ -22,35 +18,12 @@ old_node_types = df_clusters['Instance Name'].tolist()
 
 # Mapping from numerical cluster IDs to cluster names
 cluster_mapping = {
-    28: 'r6g.xlarge',
-    25: 'r6g.4xlarge',
-    22: 'r5d.large',
-    27: 'r6g.large',
-    18: 'm6g.large',
-    0: 'c5d.2xlarge',
-    24: 'r6g.2xlarge',
-    23: 'r5d.xlarge',
-    15: 'm6g.2xlarge',
-    12: 'm5d.xlarge',
-    5: 'c6g.4xlarge',
-    3: 'c5d.xlarge',
-    4: 'c6g.2xlarge',
-    19: 'm6g.xlarge',
-    2: 'c5d.9xlarge',
-    11: 'm5d.large',
-    7: 'c6g.xlarge',
-    8: 'm5d.2xlarge',
-    1: 'c5d.4xlarge',
-    16: 'm6g.4xlarge',
-    20: 'r5d.2xlarge',
-    13: 'm5zn.3xlarge',
-    6: 'c6g.8xlarge',
-    21: 'r5d.4xlarge',
-    9: 'm5d.4xlarge',
-    10: 'm5d.8xlarge',
-    14: 'm5zn.6xlarge',
-    17: 'm6g.8xlarge',
-    26: 'r6g.8xlarge'
+    28: 'r6g.xlarge', 25: 'r6g.4xlarge', 22: 'r5d.large', 27: 'r6g.large', 18: 'm6g.large',
+    0: 'c5d.2xlarge', 24: 'r6g.2xlarge', 23: 'r5d.xlarge', 15: 'm6g.2xlarge', 12: 'm5d.xlarge',
+    5: 'c6g.4xlarge', 3: 'c5d.xlarge', 4: 'c6g.2xlarge', 19: 'm6g.xlarge', 2: 'c5d.9xlarge',
+    11: 'm5d.large', 7: 'c6g.xlarge', 8: 'm5d.2xlarge', 1: 'c5d.4xlarge', 16: 'm6g.4xlarge',
+    20: 'r5d.2xlarge', 13: 'm5zn.3xlarge', 6: 'c6g.8xlarge', 21: 'r5d.4xlarge', 9: 'm5d.4xlarge',
+    10: 'm5d.8xlarge', 14: 'm5zn.6xlarge', 17: 'm6g.8xlarge', 26: 'r6g.8xlarge'
 }
 
 # Function to preprocess input data
@@ -95,10 +68,6 @@ def preprocess_input(cpu_user_percent, mem_used_percent, old_node_type, old_core
 # Streamlit UI
 st.title("Cluster Suitability Prediction")
 
-# Model selection dropdown
-selected_model_name = st.selectbox("Select Model", list(models.keys()))
-selected_model = models[selected_model_name]
-
 # Input fields
 cpu_user_percent = st.number_input("CPU User Percent", min_value=0.0, max_value=100.0, value=0.0)
 mem_used_percent = st.number_input("Memory Used Percent", min_value=0.0, max_value=100.0, value=0.0)
@@ -125,7 +94,7 @@ if st.button("Predict"):
     # Preprocess input
     preprocessed_data = preprocess_input(cpu_user_percent, mem_used_percent, old_node_type, old_core_count, old_memory_mb, old_cost)
     # Make prediction
-    prediction = selected_model.predict(preprocessed_data)
+    prediction = catboost_model.predict(preprocessed_data)
     
     # Handle prediction output
     if prediction.ndim > 1:  # Check if prediction is a 2D array
@@ -146,7 +115,8 @@ if st.button("Predict"):
         st.write(f"New Core Count: {new_core_count}")
         st.write(f"New Memory MB: {new_memory_mb}")
         st.write(f"New Cost: {new_cost}")
-    # Display cost details in a table
+        
+        # Display cost details in a table
         cost_data = {
             'Cost Type': ['AWS Cost', 'Databricks Cost'],
             'Cost': [predicted_row['On Demand Hourly Cost'].values[0], predicted_row['DBU / Hour'].values[0]]
@@ -154,7 +124,6 @@ if st.button("Predict"):
         cost_df = pd.DataFrame(cost_data)
         st.table(cost_df)
         
-
         # Calculate the difference and percentage saved
         difference = old_cost - new_cost
         percentage_saved = (difference / old_cost) * 100 if old_cost != 0 else 0
